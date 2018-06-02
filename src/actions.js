38e0bfb658ed2df32b2ca151cb3b68c0bb7c20a3
Wilbur Suero <wilbur.himself@gmail.com>
2018-06-02 09:09:47 -0400

    setup addExpense and removExpense reducers

---
+++ src/actions.js
@@ -0,0 +1,12 @@
+export const ADD_EXPENSE = 'ADD_EXPENSE'
+export const REMOVE_EXPENSE = 'REMOVE_EXPENSE'
+
+export const addExpense = expense => ({
+  type: ADD_EXPENSE,
+  expense
+})
+
+export const removeExpense = expense => ({
+  type: REMOVE_EXPENSE,
+  expense
+})
