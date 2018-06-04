2293959dbd01f1b0b35cb7e295e086bffb692959
Wilbur Suero <wilbur.himself@gmail.com>
2018-06-04 10:53:02 -0400

    add removeExpense

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { removeExpense } from '../actions'
+
+const mapDispatchToProps = dispatch => {
+  return {
+    removeExpense: expense => dispatch(removeExpense(expense))
+  }
+}
 
 const mapStateToProps = state => {
   return { expenses: state.expenses }
 }
 
-const ConnectedList = ({ expenses }) => (
-  <ul className="list-group list-group-flush">
-    {expenses.map(el => (
-      <li className="list-group-item" key={el.id}>
-        {el.title} - {el.amount}
-      </li>
-    ))}
-  </ul>
-)
+class ConnectedList extends React.Component {
 
-export default connect(mapStateToProps) (ConnectedList)
+  constructor (props) {
+    super(props)
+
+    this.removeExpense = this.removeExpense.bind(this)
+  }
+
+  removeExpense (expense) {
+    this.props.removeExpense(expense)
+  }
+
+  render () {
+    return (
+      <div>
+        <ul className="list-group list-group-flush">
+          {this.props.expenses.map(el => (
+            <li className="list-group-item" key={el.id}>
+              {el.title} - {el.amount}
+              <button onClick={() => this.removeExpense(el)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (ConnectedList)
